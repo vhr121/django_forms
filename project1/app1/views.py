99b3c63517c339cfc09fddbdc9a90f3949199776
root <root@ozl1646u.cw01.contiwan.com>
2017-12-01 11:54:28 +0530

    added a form

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -7,4 +7,8 @@ from django.shortcuts import render
 def hello(request):
  return render(request,"app1/template/index.html")
 
+def form(request):
+ return render(request,"app1/template/form.html")
+ 
+
 # Create your views here.
